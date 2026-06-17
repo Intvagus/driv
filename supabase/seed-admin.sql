b6eab4b2640cefce486ea607a4274e395ed12a6c
Intvagus <intvagus0@gmail.com>
2026-06-17 20:22:31 +0500

    Update seed-admin.sql

--- supabase/seed-admin.sql
+++ supabase/seed-admin.sql
@@ -2,4 +2,4 @@
 -- Replace the placeholder values with actual user ID and email.
 
 -- INSERT INTO admin_users (id, email, role)
--- VALUES ('<shuaib40>', 'shuaib40@mail.com', 'super_admin');
+-- VALUES ('a16b080e-b8c4-4207-a2d9-d4335a26162e', 'shuaib40@mail.com', 'super_admin');
